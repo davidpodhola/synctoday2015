ecac16c56b06551d6804a74bfb9afb0de7176eb9
David Podhola <david.podhola@naseukoly.cz>
2015-06-07 18:18:19 +0200

    Sync.Today.Models started

---
+++ src/Sync.Today.Models/Script.fsx
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#load "Library1.fs"
+open Sync.Today.Models
+
+// Define your library scripting code here
+
