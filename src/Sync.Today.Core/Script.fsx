595224cbe24bdb3f4eb2e21693b91ecbf9772209
David Podhola <david.podhola@naseukoly.cz>
2015-06-07 19:35:04 +0200

    Script fixed to show how to work with container

--- src/Sync.Today.Core/Script.fsx
+++ src/Sync.Today.Core/Script.fsx
@@ -1,8 +1,25 @@
 ﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
 // for more guidance on F# programming.
 
-#load "Library1.fs"
-open Sync.Today.Core
+#load "Container.fs"
+open IoC
 
 // Define your library scripting code here
 
+module Usage =
+
+    type ICalculate =
+        abstract member Incr : int -> int
+
+    type Calculator () =
+        interface ICalculate with
+            member this.Incr(x:int) = x + 1
+    
+    let container = Container()
+
+    container.Register<ICalculate>(typeof<Calculator>, Singleton)
+
+    let calc = container.Resolve<ICalculate>()
+    printfn "%d" (calc.Incr 1)
+
+    container.Release(calc)
