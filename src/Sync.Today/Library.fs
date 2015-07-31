dbe8403ab24ef8d4ebe58a1e0ccfcbbc4a908527
davidpodhola <david.podhola@naseukoly.cz>
2015-07-31 16:39:33 +0200

    powershell started

--- src/Sync.Today/Library.fs
+++ src/Sync.Today/Library.fs
@@ -1,22 +1,20 @@
 namespace Sync.Today
 
-/// Sync.Today
-///
-/// ## Library
-/// Sync.Today can be used as a library. Any consuming application can add the NuGet and
-/// start calling 
-/// 
-/// ## Example
-///
-///     let h = Library.hello 1
-///     printfn "%d" h
-///
-module Library = 
-  
-  /// Returns 42
-  ///
-  /// ## Parameters
-  ///  - `num` - whatever
-  let hello num = 42
+ open System  
+ open System.Management.Automation  
+ open System.ComponentModel  
+
+ [<RunInstaller(true)>]  
+ type FSharpPowerShellSnapIn() =  
+   inherit PSSnapIn()  
+   override this.Name with get() = "Sync.Today"  
+   override this.Vendor with get() = "Sync.Today Ltd"  
+   override this.Description with get() = "Sync.Today is a business processes automation platform"  
+ 
+ [<Cmdlet(VerbsCommunications.Connect, "SyncTodayService")>]  
+ type ``Connect-SyncTodayService``() =   
+   inherit Cmdlet()  
+
+// see http://apollo13cn.blogspot.cz/2013/11/use-f-to-write-powershell-snapin-cmdlet.html
 
 
